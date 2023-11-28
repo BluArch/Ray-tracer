9b0025b57c97f19cba50f5c721596aca820a0b58
JJ <92610470+BluArch@users.noreply.github.com>
2023-11-27 20:34:09 -0800

    Implemented raytracer, does not bounce or do lighting

--- RayTracer.py
+++ RayTracer.py
@@ -6,6 +6,7 @@ left = None
 right = None
 bottom = None
 top = None
+
 resolution = []
 background_color = np.array([])
 ambient = np.array([])
@@ -31,13 +32,12 @@ class Light:
 
 class Ray:
     def __init__(self, eye, direction, depth):
-        self.eye = eye
-        self.direction = direction
+        self.eye = np.array(eye)
+        self.direction = np.array(direction)
         self.depth = depth
     
     def set_depth(self, new_depth):
         self.depth = new_depth
-
 
 def parse_info():
     # Takes input from command line for the variables of the scene
@@ -98,11 +98,54 @@ def output_ppm(filename, image):
             # Add a newline after each row
             output.write('\n')
 
+def raytrace(currentRay: Ray):
+    # if (currentRay.depth > 3):
+    #     return np.array([0,0,0])
+    intersect = closest_intersect(currentRay, spheres)
+    if intersect is not None:
+        return intersect.color
+        print("intersect made, setting color to current sphere color")
+    else:
+        return background_color
+
+def closest_intersect(currentRay: Ray, spheres_list):
+    # closest_intersect = float("inf")
+    # closest_sphere: Ray
+    count = 1
+    for sphere in spheres_list:
+        # print("printing sphere " + str(count))
+        eye_minus_center = currentRay.eye - sphere.position
+
+        # Calculate A,B,C values
+        A = np.dot(currentRay.direction, currentRay.direction)
+        B = 2.0 * np.dot(currentRay.direction, eye_minus_center)
+        C = np.dot(eye_minus_center, eye_minus_center) - 1
+
+        # Solve for the discriminant, 
+        discriminant = B**2 - (A*C)
+        if (discriminant < 0 or discriminant == 0):
+            return None
+        discriminant = np.sqrt(B**2 - (A*C))
+
+        t1 = (-B/A) + (discriminant/A)
+        t2 = (-B/A) - (discriminant/A)
+
+        if (t1>0):
+            return sphere
+        if (t2>0):
+            return sphere
+        count=+1
+    return None
+    
 
 def main():
     parse_info()
 
-    eye = np.array([0,0,5])
+    # For image plane settup
+    W = resolution[1]/2
+    H = resolution[0]/2
+
+    eye = np.array([0,0,-5])
     lookat = np.array([0,0,0])
     up = np.array([0,1,0])
 
@@ -120,18 +163,20 @@ def main():
     for pixel_column in range(resolution[0]):
         for pixel_row in range(resolution[1]):
             # Finding lower left uc and vr values in camera coordinate system
-            uc = -left + (left * (2*pixel_column)/(resolution[0]))
-            vr = -right + (right * (2*pixel_row)/(resolution[1]))
+            uc = -W + (W * (2*pixel_column)/(resolution[0]))
+            vr = -H + (H * (2*pixel_row)/(resolution[1]))
 
             # Pixel in image plane relative to camera coordinate system
             pixel_in_camera = eye - near*n + uc*u + vr*v
 
             # Create current ray
             curr_ray = Ray(eye, pixel_in_camera - eye, 0)
+            curr_ray.set_depth(1)
 
             # curr_pixel_color = raytrace()
-            backgroundcolor = background_color * 255
-            image[pixel_row, pixel_column] = backgroundcolor  # Set current pixel color
+            colorCR = raytrace(curr_ray)
+            color = colorCR * 255
+            image[pixel_row, pixel_column] = color  # Set current pixel color
 
     output_ppm(output_name, image)
 
