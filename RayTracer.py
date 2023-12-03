9963e77f673c5c72a4fc40e31ca841cb6928c96b
JJ <92610470+BluArch@users.noreply.github.com>
2023-12-03 15:24:40 -0800

    Lighting is partially functional, diffuse seems to be a bit dark

--- RayTracer.py
+++ RayTracer.py
@@ -9,7 +9,7 @@ bottom = None
 top = None
 output_name = None
 resolution = []
-ambient = []
+ambient = np.array([])
 spheres = []
 lights = []
 background_color = np.array([])
@@ -88,18 +88,27 @@ def parse_info():
             output_name = str(values[1])
 
 
-def normalize(vector):
+def normalize(vector: np.array):
     """
     Function normalizes a given vector and returns the normalized vector
     """
     return vector / np.linalg.norm(vector)
 
-def inverse_by_scale(vector, scale):
+def inverse_by_scale(vector: np.array, scale: np.array):
     """
     Function computes the dot product of a vector and a matrix, scales the vector by the inverse of the scaling matrix
     """
     inverse_scale =  np.linalg.inv(scale)
     return np.dot(vector, inverse_scale)
+
+def inverse_transpose(vector: np.array, scale: np.array):
+    """
+    Function computes the inverse transpose of a vector by a given scale
+    """
+    inverse = np.linalg.inv(scale)
+    transpose = np.transpose(inverse)
+    return np.dot(vector, transpose)
+
 
 def raytrace(currentRay: Ray):
     """
@@ -108,20 +117,73 @@ def raytrace(currentRay: Ray):
     # If we've reached max depth, return black
     if (currentRay.depth > 3):
         return np.array([0,0,0])
+    
     # Search for whether there is a closest object to intersect with
     closest_object, closest_distance = find_closest_intersection(currentRay)
+
     # If no object, return background color
     if (closest_object is None):
         return background_color
-    # Calculates intersect point of minimum distance object
-    P = currentRay.eye - closest_distance*currentRay.direction
-
-    return closest_object.color
     
-def find_closest_intersection(curr_ray):
+    # closest_distance = np.dot(closest_distance, closest_object.scale)
+    # Calculates intersect point of minimum distance object
+    P = currentRay.eye + closest_distance*currentRay.direction
+    # Calculates the normal
+    # After this point it gets sketchy
+    p_normal = normalize(P-closest_object.position)
+    p_normal = inverse_transpose(p_normal, closest_object.scale)
+    P = P + 0.000001 * p_normal
+
+
+    # Compute ambient lighting + lighting from lights
+    clocal = color_from_lights(currentRay.eye, P, p_normal, closest_object)
+
+    reflected_direction = 2 * np.dot(p_normal, currentRay.direction)*p_normal - currentRay.direction
+    reflected_ray = Ray(P, reflected_direction, currentRay.depth + 1)
+
+    colorRE = raytrace(reflected_ray)
+
+    pixel_color = clocal + colorRE*closest_object.coefficients[3]
+    return pixel_color
+    
+def color_from_lights(eye: np.array, point: np.array, normal: np.array, object: Sphere):
+    """
+    TODO: get this to work
+    Function returns the sum of all lighting from lights using ADS model
+    """
+    # Get vector from eye to point
+    point_to_eye = normalize(eye - point)
+    clocal = ambient * object.color * object.coefficients[0]
+
+    for light in lights:
+        # Get vector to current light from point
+        point_to_light = normalize(light.position - point)
+        # Convert light ray to Ray object to run find_closest_intersection on it
+        light_ray = Ray(point, point_to_light, 0)
+
+        _, closest_distance = find_closest_intersection(light_ray)
+        distance_to_light = np.linalg.norm(light.position - point)
+
+        if (distance_to_light > closest_distance):
+            continue
+
+        # Calculate diffuse value
+        diffuse = object.coefficients[1] * light.color * np.dot(normal, point_to_light) * object.color
+
+        # Calculate specular value
+        R = 2.0*np.dot(normal, point_to_light)*normal - point_to_light
+        specular = object.coefficients[2] * light.color * np.dot(R, point_to_eye)**object.shine
+
+        clocal += diffuse + specular
+    return clocal
+
+
+
+def find_closest_intersection(curr_ray: Ray):
     """
     Computes all intersections of a given ray with the objects in the scene.
     Return the closest object with the distance to the object
+    TODO: turn back on inverses
     """
     closest_object = None
     closest_distance = np.inf
@@ -132,20 +194,19 @@ def find_closest_intersection(curr_ray):
         # Find the scaled value of the ray and vector from eye to circle center
         curr_ray_inverse = inverse_by_scale(curr_ray.direction, curr_sphere.scale)
         eye_center_inverse = inverse_by_scale(eye_minus_center, curr_sphere.scale)
-
+        # Solve for a, b, and c values
         a = np.dot(curr_ray_inverse, curr_ray_inverse)
         b = 2 * np.dot(curr_ray_inverse, eye_center_inverse)
         c = np.linalg.norm(eye_center_inverse)**2 - 1
         # Find the number of intersections
         discriminant = b**2 - 4*a*c
+        t = None
         if(discriminant>0):
             discriminant = np.sqrt(discriminant)
             t1 = (-b + discriminant) / (a*2)
             t2 = (-b - discriminant) / (a*2)
-            if (t1>0 and t2>0):
+            if (t1>near and t2>near):
                 t = min(t1, t2)
-        else:
-            t = None
         # Update values if closer
         if (t and t<closest_distance):
             closest_distance = t
@@ -159,7 +220,7 @@ def main():
     """
     parse_info()
     # Set up eye and image
-    eye = np.array([0,0,near])
+    eye = np.array([0,0,0])
     image = np.zeros((resolution[1], resolution[0], 3))
 
     # Iterate through every pixel of the image
@@ -169,7 +230,7 @@ def main():
             uc = left + (right*(2*pixel_column)/(resolution[1]))
             vr = top + (bottom*(2*pixel_row)/(resolution[0]))
             # Get direction of camera to pixel
-            pixel = np.array([uc,vr,0])
+            pixel = np.array([uc,vr,-near])
             direction = normalize(pixel - eye)
             # Raytrace on current pixel and update pixel color
             color = raytrace(Ray(eye, direction, 1))
